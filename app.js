fe1220154907f1df9dec400198afdd31d1962f3a
alex-sir <alexcarbajal710@gmail.com>
2019-03-27 23:34:44 -0700

    Personal Portfolio

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("portfolio-main");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
+
+app.listen(3000, () => {
+    console.log("SERVER STARTED");
+});
