180984dab3165007780c93980264a7b871a793de
alex-sir <alexcarbajal710@gmail.com>
2019-03-29 23:48:12 -0700

    Body content, arrow, & cross-compatibility

--- public/js/root_content_logic.js
+++ public/js/root_content_logic.js
@@ -1,16 +1,5 @@
-function selectSubject() {
-    const subjects = document.querySelectorAll(".subject")
-
-    for (let i = 0; i < subjects.length; i++) {
-        subjects[i].addEventListener("click", function () {
-            for (let j = 0; j < subjects.length; j++) {
-                subjects[j].classList.remove("selected");
-                subjects[j].classList.add("unselected");
-            }
-            this.classList.remove("unselected");
-            this.classList.add("selected");
-        });
-    }
-}
-
-selectSubject();
+$("#arrow").click(function() {
+    $("html, body").animate({
+        scrollTop: $("#skills").offset().top
+    }, 800);
+});
